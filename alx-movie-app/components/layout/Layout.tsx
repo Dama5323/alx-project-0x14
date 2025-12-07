c993e7f80819dda53c353180995eab4a5fa3fbc9
Dama5323 <your-deenyashke@gmail.com>
2025-12-07 10:06:04 +0300

    Bootstrap MovieApp

---
+++ alx-movie-app/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+// components/layouts/Layout.tsx
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
